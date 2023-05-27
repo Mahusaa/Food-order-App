b484ab01a285cd7947d6a86296c6e1dc89aaf0d4
usamah <hakaro375@gmail.com>
2023-05-27 21:31:14 +0700

    adding hide and shown cart with button

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,8 +4,8 @@ import classes from './Cart.module.css';
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item, index) => (
+        <li key={index}>{item.name}</li>
       ))}
     </ul>
   );
@@ -18,7 +18,7 @@ const Cart = (props) => {
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,20 +1,25 @@
 import React, { Fragment } from 'react';
-import classes from './Header.module.css'
+import classes from './Header.module.css';
 import mealsImage from '../../Assets/food-and-bevarge-header.jpg'
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
-    return (
-        <Fragment>
-        <header className={classes.header}>
+const Header = (props) => {
+  const cartButtonHandler = () => {
+    props.onShowCart()
+    console.log("Button got clicked");
+  };
+
+  return (
+    <Fragment>
+      <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
-        </header>
-        <div className={classes['main-image']} alt="">
-           <img src={mealsImage} alt="mealsImage"/>
-        </div>
-        </Fragment>
-    );
+        <HeaderCartButton onClick={cartButtonHandler} />
+      </header>
+      <div className={classes['main-image']} alt="">
+        <img src={mealsImage} alt="mealsImage" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
