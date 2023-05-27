4779701c4f6382ad0651f77a23f6dac5fa1a2a16
usamah <hakaro375@gmail.com>
2023-05-27 21:38:04 +0700

    adding hide cart using backdrop

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -3,12 +3,12 @@ import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onHideCart}></div>;
 };
 const Overlays = (props) => {
   return (
     <div className={classes.modal}>
-      <div className={classes.content}>{props.children}</div>
+      <div className={classes.content}>{props.children} </div>
     </div>
   );
 };
@@ -17,7 +17,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onHideCart={props.onHideCart}/>, portalElement)}
       {ReactDOM.createPortal(
         <Overlays>{props.children}</Overlays>,
         portalElement
